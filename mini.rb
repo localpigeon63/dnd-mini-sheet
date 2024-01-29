3a091b713e34197ae874f72657c518fa471f513a
Waldemar <34974+Velrok@users.noreply.github.com>
2024-01-29 23:24:42 +0000

    calc modifiers

--- mini.rb
+++ mini.rb
@@ -2,26 +2,30 @@
 # typed: strong
 require 'sinatra'
 
+#[7,8,9, 11,12, 20].map {|s| (s - 10) /2}
 get '/' do
   # data = {:name => "Daisy"}
-  data = {
-    name: "Daisy",
-    attributes: {
+    attributes = {
       str: 17,
       dex: 7,
       con: 12,
       int: 13,
       wis: 16,
       cha: 12
-    },
-    modifiers: {
-      str: 3,
-      dex: -2,
-      con: 1,
-      int: 1,
-      wis: 3,
-      cha: 1
     }
+
+  modifiers = attributes.map do |stat,score|
+    modifier = (score - 10) /2
+    [stat, modifier]
+  end.to_h
+  # [3,-2,1,1,4,1]
+  # [[:str, 3], [:dex, -2]]
+
+  data = {
+    name: "Daisy",
+    attributes: attributes,
+    modifiers: modifiers
   }
+
   erb(:index, {locals: data})
 end
